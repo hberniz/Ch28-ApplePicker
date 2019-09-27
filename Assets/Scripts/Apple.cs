75ef7d29a7376184e861a34f09e63f9d1c2b4d32
hberniz <55005292+hberniz@users.noreply.github.com>
2019-09-27 01:28:45 -0400

    functional working apple picker

--- Assets/Scripts/Apple.cs
+++ Assets/Scripts/Apple.cs
@@ -1,14 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class Apple : MonoBehaviour
 {
-    public static float bottomY = -20f;
-    // Start is called before the first frame update
+    public static float bottomY = -15f;
+    // Use this for initialization
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,7 +16,10 @@ public class Apple : MonoBehaviour
         if (transform.position.y < bottomY)
         {
             Destroy(this.gameObject);
+            // Get a reference to the ApplePicker component of Main Camera
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>(); // 1
+                                                                            // Call the public AppleDestroyed() method of apScript
+            apScript.AppleDestroyed();                                      // 2
         }
     }
-
 }
--- Assets/Scripts/AppleTree.cs
+++ Assets/Scripts/AppleTree.cs
@@ -1,20 +1,23 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
+using System.Collections;
 
 public class AppleTree : MonoBehaviour
 {
     // Prefab for instantiating apples
     public GameObject applePrefab;
+
     // Speed at which the AppleTree moves in meters/second
     public float speed = 1f;
+
     // Distance where AppleTree turns around
-    public float leftAndRightEdge = 10f;
+    public float leftAndRightEdge = 20f;
+
     // Chance that the AppleTree will change directions
     public float chanceToChangeDirections = 0.1f;
+
     // Rate at which Apples will be instantiated
     public float secondsBetweenAppleDrops = 1f;
+    // Use this for initialization
     void Start()
     {
         // Dropping apples every second
@@ -25,36 +28,29 @@ public class AppleTree : MonoBehaviour
         GameObject apple = Instantiate(applePrefab) as GameObject;
         apple.transform.position = transform.position;
     }
-
-void Update()
+    // Update is called once per frame
+    void Update()
     {
-        // Basic Movement
         // Basic Movement
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
-        //pos.x += 1.0f * 0.04f;
-        //pos.x += 0.04f;
         transform.position = pos;
         // Changing Direction
         if (pos.x < -leftAndRightEdge)
         {
-            speed = Mathf.Abs(speed); // Move right
+            speed = Mathf.Abs(speed);  // Move right
         }
         else if (pos.x > leftAndRightEdge)
         {
             speed = -Mathf.Abs(speed); // Move left
         }
-        
-
-
     }
     void FixedUpdate()
     {
         // Changing Direction Randomly
         if (Random.value < chanceToChangeDirections)
         {
-            speed *= -1; // Change direction
+            speed *= -1;  // Change direction
         }
     }
-
 }
